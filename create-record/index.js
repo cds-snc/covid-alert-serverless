fa33155eaaec456d5360af9914dbd7c40b30bc1f
Tim jodoin <tim.jodoin@gmail.com>
2021-01-27 12:39:37 -0500

    fix adjusted filename var to const and removed nodeunit from dependancies

--- create-record/index.js
+++ create-record/index.js
@@ -10,7 +10,7 @@ exports.handler = async (event, context) => {
     const bucket = process.env.dataBucket;
     const filePath = process.env.fileLoca;
     const kmsKey = process.env.dataKey;
-    let filename = uuid.v1();
+    const filename = uuid.v1();
     let transactionStatus = "FAILED";
     var body = JSON.stringify(event);
 
@@ -22,12 +22,13 @@ exports.handler = async (event, context) => {
         SSEKMSKeyId: kmsKey
     };
 
+    /* The puObject call forces a promise because the result returned may not be a promise.  */
     try {
-        let resp = await S3.putObject(bucketParams).promise();
-        console.log(resp);
+        const resp = await S3.putObject(bucketParams).promise();
+        console.debug(resp);
         transactionStatus = { "status": "RECORD_CREATED", "key": filename};
     } catch (err) {
-        console.log("UPLOAD FAILED", err);
+        console.debug("UPLOAD FAILED", err);
         transactionStatus = { "status": "UPLOAD FAILED"};
     }
 
